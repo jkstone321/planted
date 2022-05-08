22cf2c608be4b77ce24f932a3251a4081bd6178c
Odis Clemons <odis.clemons@gmail.com>
2022-05-08 16:25:41 -0400

    grid system is now customizable and seperated in a seperate module

--- gridTest/assets/js/script.js
+++ gridTest/assets/js/script.js
@@ -2,43 +2,18 @@ var gridItems = []
 var currentColor = "red"
 var inactiveColor = "white"
 var mouseDown = null;
+var gridSquareSize = '.5rem'
+
+// change to true to show grid coordinates for the squares
+var coordinatesOn = false
 
 const gridDimensions = [
-    { x: 28, y: 8 },
+    { x: 100, y: 90 },
     { x: 16, y: 9 }
 ]
-// class for each new grid item to be saved in gridItems array
-class GridItem {
-    constructor(column, row) {
-        this.column = column
-        this.row = row
-        this.selected = false
-        this.plant = ""
-        this.selectedColor = currentColor
-    }
-
-    setColor(value) {
-        this.selectedColor = value
-        return this
-    }
-
-    //dont toggle it. explicitly set it to true or false
-    setSelected(value) {
-        this.selected = value
-        return this
-    }
-
-    toggle() {
-        // whatever the current value of selected is,
-        // set it to the opposite and return the updated object
-        this.selected = !this.selected
-        if (this.selected && this.selectedColor !== currentColor) this.setColor(currentColor)
-        return this
-    }
-}
 
 // draw the grid
-async function handleDrawGrid(index) {
+async function handleDrawGrid() {
     //save the grid container to variable
     const gridContainer = $('#gridContainer')
     // empty it if theres anything in there
@@ -51,43 +26,32 @@ async function handleDrawGrid(index) {
     var { x, y } = gridDimensions[0]
 
     // set the grid-template-columns to repeat 
-    gridContainer.css("grid-template-columns", `1fr repeat(${x - 1}, 2.5rem)`)
+    gridContainer.css("grid-template-columns", `1fr repeat(${x - 1}, ${gridSquareSize})`)
     for (let i = 0; i < (x * y); i++) {
         // check which row we're on by doing i mod x
         // if it returns anything other than 0 we're not at
         // the end of the row yet
         if (i >= x && i % x == 0) { row++; column = 0 }
 
-        // make a unique id for each row item
-        const gridId = `${i}-${row}`
+        let currentGi = new GridItem(column, row, i)
 
-        // the first time this function is run, we call it with no index specified
-        // if there is no index, create a new GridItem.
-        // otherwise this function has an index and the array of griditems
-        // is already created so just get that item from the array
-        let currentGi = index ? gridItems[i] : new GridItem(column, row)
+        gridContainer.append(currentGi.gridSquare)
 
-        // if the specified index matchs the loop number we're on,
-        // toggle that item
-        if (index === i) currentGi = gridItems[i].toggle()
-
-        // create the grid square and add it to gridContainer
-        let gi = `<div id="${gridId}" class="grid" style="background-color: ${currentGi.selected ? currentGi.selectedColor : inactiveColor}" onClick="handleDrawGrid(${i})" onmouseover="handleMouseOver(${i})">${gridId}</div>`
-        gridContainer.append(gi)
-
-        if (!index) gridItems.push(currentGi)
+        gridItems.push(currentGi)
 
         column++
     }
     return
 }
 
+//loop through each grid item and clear them
 function clearGrid() {
     gridItems = gridItems.map(gi => gi.setSelected(false))
     handleDrawGrid()
 }
 
-function handleColorPalette(e) {
+// will rename this.  this is triggered when they click setColor button
+function handleSetColor(e) {
     e.preventDefault()
     let newColor = $('#inpCurrentColor').val()
     let currentColorDisplay = $('#currentColorDisplay')
@@ -96,25 +60,18 @@ function handleColorPalette(e) {
     currentColor = newColor
 }
 
-function handleMouseOver(index) {
-    return
-    console.log(index, mouseDown)
-    if (mouseDown === 0) handleDrawGrid(index)
-}
-
 $(document).ready(() => {
     var currentColorDisplay = $('#currentColorDisplay')
     currentColorDisplay.css({ backgroundColor: currentColor })
     let frmColorPalette = $('#frmColorPalette')
-    frmColorPalette.on('submit', handleColorPalette)
-    handleDrawGrid().then(() => console.log(gridItems))
+    frmColorPalette.on('submit', handleSetColor)
+    handleDrawGrid().then(() => console.log('# of gridItems:', gridItems.length))
 
-    // document.body.onmousedown = function (e) {
-    //     mouseDown++;
-
-    // }
-    // document.body.onmouseup = function (e) {
-    //     mouseDown--
-    // }
-
+    document.body.onmousedown = function () {
+        ++mouseDown;
+    }
+    document.body.onmouseup = function () {
+        --mouseDown;
+    }
 })
+
