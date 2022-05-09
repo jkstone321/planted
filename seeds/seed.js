22cbc976483c8d352511c3c9b13d385013d9c971
Droessling94 <droessling94@gmail.com>
2022-05-09 13:08:29 -0400

    updated garden routes to be plant routes, updated as much 'garden' tags as i could, check if api routes work and we look good

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Garden } = require('../models');
+const { User, Plant } = require('../models');
 
 const userData = require('./userData.json');
-const gardenData = require('./gardenData.json');
+const plantData = require('./plantData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const garden of gardenData) {
-    await Garden.create({
-      ...garden,
+  for (const plant of plantData) {
+    await Plant.create({
+      ...plant,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
