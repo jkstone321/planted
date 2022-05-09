4ed6836fa5cd6817756c3ef9c726166f2183cb64
Joshua Diaz <jad24096@gmail.com>
2022-05-09 11:22:39 -0400

    wokred on models, seeds, db.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Garden } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const gardenData = require('./gardenData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const garden of gardenData) {
+    await Garden.create({
+      ...garden,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
