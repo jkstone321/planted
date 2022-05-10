7965b4e3a54ae90fc8b5dcb5576bbbc8fcba979e
jkstone321 <jkstone321@gmail.com>
2022-05-10 11:54:39 -0400

    changed login and signups redirectly to mygardens page instead of a nonexistant page

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace('/mygarden');
     } else {
       alert(response.statusText);
     }
@@ -37,7 +37,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/mygarden');
     } else {
       alert(response.statusText);
     }
