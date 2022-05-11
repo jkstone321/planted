e5b5efed7ae7aca268c99f5ed2c98b232fc37ca4
Droessling94 <101940633+Droessling94@users.noreply.github.com>
2022-05-11 15:07:10 -0400

    user chosenplatns goto DB now

--- public/js/addtolistPOST.js
+++ public/js/addtolistPOST.js
@@ -6,14 +6,25 @@ async function userPlantsPOST(){
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
       });
+      console.log(chosenPlantDataBaseRaw);
       let chosenPlantDataBaseJson = await chosenPlantDataBaseRaw.json();
-      let myChosenPlantsArray = chosenPlantDataBaseJson.chosen_plant
+      console.log(chosenPlantDataBaseJson);
+      let myChosenPlantsArray = chosenPlantDataBaseJson
       let myNewPlantName = document.getElementById("myInput").value
       let myNewPlant = {type:`${myNewPlantName}`, color:""};
       console.log(myNewPlant);
       myNewPlant = JSON.stringify(myNewPlant)
-      console.log(myChosenPlantsArray.slice(0,-1));
+      console.log(myNewPlant);
       myChosenPlantsArray = myChosenPlantsArray.slice(0,-1)
+      if(!myChosenPlantsArray) {
+        myNewChosenPlantsArray = await myChosenPlantsArray.concat(myNewPlant+"]");
+        console.log(myNewChosenPlantsArray);
+      const newChosenPlantDataBaseRaw = await fetch('/api/users/mylist',{
+          method:'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body:myNewChosenPlantsArray,
+      }).catch(err => {console.log(err); alert(err.msg)})
+      }else{
       myNewChosenPlantsArray = await myChosenPlantsArray.concat(","+myNewPlant+"]");
       console.log(myNewChosenPlantsArray);
     const newChosenPlantDataBaseRaw = await fetch('/api/users/mylist',{
@@ -21,6 +32,7 @@ async function userPlantsPOST(){
         headers: { 'Content-Type': 'application/json' },
         body:myNewChosenPlantsArray,
     }).catch(err => {console.log(err); alert(err.msg)})
+};
 
     // get error from fetch request
 }
