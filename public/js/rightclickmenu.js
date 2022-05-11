1a5f8abf16a2f4cfebfb88b2c3b55b77d0263ca3
Droessling94 <101940633+Droessling94@users.noreply.github.com>
2022-05-11 05:47:51 -0400

    update autocomplete funciton to pull form DB incase we add more or something, rightclick menu is pooched but the frame work is there, eventdelegation just getting me

--- public/js/rightclickmenu.js
+++ public/js/rightclickmenu.js
@@ -6,6 +6,9 @@ const rightClickMenu = document.getElementById("rightClickMenu");
 //--SCOPE IS NEEDED TO BE ABLE TO CLICK OUTSIDE OF THE CONTEXT MENU AND CLOSE IT--//
 const scope = document.querySelector("body");
 
+
+document.body.addEventListener( 'click', function ( event ) {
+    if( event.target.id == 'rightClickMenu' ) {
 //--FOREACH METHOD AVAILABLE TO NODELISTS, ADDS THE CALLBACK FUNCTION TO EACH ELEMENT--//
 myChosenPlant.forEach(plant => {
     //==RIGHTCLICK MENU OPEN==//
@@ -19,10 +22,16 @@ myChosenPlant.forEach(plant => {
         rightClickMenu.classList.add('visible');
     });
 })
+    };
+  } );
 
-//==CLICK AWAY MENU CLOSE==//
+
+document.body.addEventListener( 'click', function ( event ) {
+    if( event.target.id == 'rightClickMenu' ) {
+        //==CLICK AWAY MENU CLOSE==//
 scope.addEventListener('click', (e) => {
     if (e.target.offsetParent != rightClickMenu) {
         rightClickMenu.classList.remove('visible');
     }
 })
+    }});
