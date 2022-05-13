624b843fc39abcd4d4b6f876f532c01cad3102c4
jkstone321 <jkstone321@gmail.com>
2022-05-13 16:50:34 -0400

    trying a solution from stack

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -1,9 +1,15 @@
+let plantNames = [];
+
 $(document).ready(async function () {
   const plantNamesData = await fetch('/api/plants/name', {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   });
-  let plantNames = await plantNamesData.json();
+  plantNames = await plantNamesData.json();
+
+  jQuery.fn.extend({
+    propAttr: $.fn.prop || $.fn.attr,
+  });
   $('#myInput').autocomplete({
     source: plantNames,
     maxShowItems: 10,
