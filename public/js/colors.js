c2d8678a2ea11987674fa56bec4cf0706c570e1c
Odis Clemons <odis.clemons@gmail.com>
2022-05-15 12:35:35 -0400

    deleting colors works for rgb colors only.  We will have to replace all the colors with rgb

--- public/js/colors.js
+++ public/js/colors.js
@@ -1,5 +1,10 @@
 // // 148 colors total
 var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 255, 255)",
+    "rgb(0, 0, 0)",
     "AliceBlue",
     "AntiqueWhite",
     "Aqua",
